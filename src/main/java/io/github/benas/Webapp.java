4e46d1256754bef350fab54d262844e8ada133e7
Mahmoud Ben Hassine <mahmoud.benhassine@icloud.com>
2016-04-20 14:59:55 +0200

    refactoring

--- src/main/java/io/github/benas/Webapp.java
+++ src/main/java/io/github/benas/Webapp.java
@@ -2,12 +2,14 @@ package io.github.benas;
 
 import org.apache.catalina.startup.Tomcat;
 
+import static java.lang.Integer.parseInt;
+
 public class Webapp {
 
     public static void main(String[] args) throws Exception {
-        String contextPath = args[0];
-        String appBase = args[1]; // absolute path to target/${project.artifactId}-${project.version}
-        int port = Integer.parseInt(args[2]);
+        String contextPath = "";
+        String appBase = args[0]; // absolute path to target/${project.artifactId}-${project.version}
+        int port = args.length > 1 ? parseInt(args[1]) : 8080;
 
         Tomcat tomcat = new Tomcat();
         tomcat.setPort(port);
@@ -15,7 +17,7 @@ public class Webapp {
         tomcat.addWebapp(contextPath, appBase);
         tomcat.start();
 
-        System.out.println("Waiting for requests on http://localhost:" + port);
+        System.out.println("Waiting for requests on http://localhost:" + port + contextPath + "/");
         tomcat.getServer().await();
     }
 }
--- src/main/java/io/github/benas/servlet/IndexServlet.java
+++ src/main/java/io/github/benas/servlet/IndexServlet.java
@@ -1,20 +1,17 @@
 package io.github.benas.servlet;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet("/")
 public class IndexServlet extends HttpServlet {
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/plain");
-        PrintWriter out = response.getWriter();
-        out.print("welcome!");
+        response.getWriter().println("welcome!");
     }
     
 }
